1175b9dd047353fb3adbcf2d0182c33d1ba364e7
coconan <coconan8@gmail.com>
2021-12-16 17:49:24 +0800

    read events

--- src/me/coconan/agiletodo/Application.java
+++ src/me/coconan/agiletodo/Application.java
@@ -1,6 +1,6 @@
 package me.coconan.agiletodo;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * mkdir -p out
@@ -13,10 +13,24 @@ public class Application {
         // get input directory
         String directory = args[0];
         System.out.printf("directory: %s\n", directory);
+
         // list directory files
         File dir = new File(directory);
         for (String filename : dir.list()) {
             System.out.println(filename);
+        }
+
+        // find spring events.md
+        File events = new File(directory + "/pearls/spring/events.md");
+        System.out.println(events.getAbsolutePath());
+        System.out.println("exists: " + events.exists());
+
+        // read file content
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(events));
+            System.out.println(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
