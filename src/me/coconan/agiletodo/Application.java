5894688de792cc63be9bf01e415fe9d717e019d0
coconan <coconan8@gmail.com>
2021-12-16 17:40:34 +0800

    list directory

--- src/me/coconan/agiletodo/Application.java
+++ src/me/coconan/agiletodo/Application.java
@@ -1,9 +1,11 @@
 package me.coconan.agiletodo;
 
+import java.io.File;
+
 /**
  * mkdir -p out
- * javac src/me/coconan/agiletodo/Application.java -d out
- * java -classpath out me.coconan.agiletodo.Application ~/enduring-patience
+ * javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar src/me/coconan/agiletodo/Application.java -d out
+ * java -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out me.coconan.agiletodo.Application ~/enduring-patience
  */
 public class Application {
 
@@ -11,6 +13,11 @@ public class Application {
         // get input directory
         String directory = args[0];
         System.out.printf("directory: %s\n", directory);
+        // list directory files
+        File dir = new File(directory);
+        for (String filename : dir.list()) {
+            System.out.println(filename);
+        }
     }
 
 }
